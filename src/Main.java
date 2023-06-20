88cb36d5efcb40ddeb37fd9b6d09827ee323fb0b
anshp <anshparekh03@gmail.com>
2023-06-21 02:07:58 +0530

    origin

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+            Integer x1=400, x2=400;
+            // Both x1 and x2 are objects referring to different things
+            if(x1==x2){
+                System.out.println("Same");
+            }
+            else{
+                System.out.println("Not Same");
+            }
+        }
+    }
